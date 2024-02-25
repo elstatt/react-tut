a04460994128f2d87f9397fc4fa7ac54a90a521a
elstatt <kolsa.medrua@gmail.com>
2024-02-25 22:25:59 +0530

    created components

--- react1/task1/p1/my-app/src/App.js
+++ react1/task1/p1/my-app/src/App.js
@@ -1,20 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import MyInfo from './components/MyInfo';
 
-function MyInfo() {
-  return (
-    <div class="punk effect">
-      <h1>Kolsa</h1>
-      <p>I am a 10x eng. from the city of galegerda</p>
-      <h3>My future vacation spots</h3>
-      <ul>
-        <li>mars</li>
-        <li>galagtus</li>
-        <li>Tesseract 1b</li>
-      </ul>
-    </div>
-  )
-}
 
 function KyokaInfo() {
   return (
---
+++ react1/task1/p1/my-app/src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+function MyInfo() {
+    return (
+      <div class="punk effect">
+        <h1>Kolsa</h1>
+        <p>I am a 10x eng. from the city of galegerda</p>
+        <h3>My future vacation spots</h3>
+        <ul>
+          <li>mars</li>
+          <li>galagtus</li>
+          <li>Tesseract 1b</li>
+        </ul>
+      </div>
+    )
+  }
+export default MyInfo;
