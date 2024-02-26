4fcf80191478e96e6b21cec9061c2e6702c1c1fd
elstatt <kolsa.medrua@gmail.com>
2024-02-26 14:52:30 +0530

    TODO App created

--- react1/task1/p1/sep-file/src/App.js
+++ react1/task1/p1/sep-file/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from "./Components/Navbar"
+import MainContent from "./Components/MainContent"
+import Footer from "./Components/Footer"
 function App() {
   return (
     <div className="App">
