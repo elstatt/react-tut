ec173f5abb81313081598abd07e1877b7eb1038e
elstatt <kolsa.medrua@gmail.com>
2024-02-26 12:29:48 +0530

    Added Navbar & MainContent

---
+++ react1/task1/p1/sep-file/src/Components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Welcome to the main content of the page</h1>
+            
+        </div>
+    )
+}
+
+export default MainContent;
---
+++ react1/task1/p1/sep-file/src/Components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <header>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        About Us
+                    </li>
+                    <li>
+                        Product
+                    </li>
+                    <li>
+                        Other Cos
+                    </li>
+                </ul>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar;
