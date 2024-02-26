9c32d7dd2b7ae53e1ccb23d7cf3f0c8f44a4d64d
elstatt <kolsa.medrua@gmail.com>
2024-02-26 12:34:10 +0530

    Added Footer

---
+++ react1/task1/p1/sep-file/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>Contact me on github @elstatt</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
--- react1/task1/p1/sep-file/src/Components/MainContent.js
+++ react1/task1/p1/sep-file/src/Components/MainContent.js
@@ -3,8 +3,14 @@ import React from 'react'
 function MainContent() {
     return (
         <div>
+            <main>
             <h1>Welcome to the main content of the page</h1>
-            
+            <p>This is where we actually plan how things are going to go</p>
+            <p>Hello I am a 10x developer from the humble abode of galengatis</p>
+            <textarea rows="4" cols="50">
+                There are ways that we can make the world a better place, one is to improve education for everyone, what do you think can you suggest more?
+            </textarea>
+            </main>
         </div>
     )
 }
